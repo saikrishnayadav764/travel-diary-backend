700c310a1be18829a716ba3a4693d000f32b69a1
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2024-04-14 23:20:37 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,12 +2,16 @@
 
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, userProfile } = require('../controllers/authController');
+const authenticate = require("../config/middleware");
 
 // POST /api/auth/register
 router.post('/register', register);
 
 // POST /api/auth/login
 router.post('/login', login);
+
+// GET /api/auth/profile
+router.get("/profile", authenticate, userProfile);
 
 module.exports = router;
