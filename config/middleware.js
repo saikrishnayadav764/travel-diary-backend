eae8db40310ee0a2bf3b2a1fab3d819e718f4329
saikrishnayadav764 <106056182+saikrishnayadav764@users.noreply.github.com>
2024-04-15 12:16:19 +0530

    Update middleware.js

--- config/middleware.js
+++ config/middleware.js
@@ -1,6 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 function authenticate(req, res, next) {
+  
+  const hp = req.header("Authorization");
+
+  if (!hp) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   const token = req.header("Authorization").split(" ")[1];
 
   if (!token) {
